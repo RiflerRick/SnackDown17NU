756788c1677fef6413cfd4bb081f4a2e7d4c903c
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-05-28 08:39:05 +0530

    modified snakeEating

--- SnakeEating.py
+++ SnakeEating.py
@@ -2,29 +2,29 @@ def answer(l, k):
     # lengthList is a list of lengths of all the snakes
     lengthList=l[:]
     num=0
-
+ 
     # sorting the list in ascending order 
     lengthList.sort()
     index=len(lengthList)-1
     while index>0:
         # traversing the list from back to front
-        if lengthList[index]>=k:
+        if int(lengthList[index])>=k:
             num+=1
-
+ 
             # pop the length of the snake that is already going to be greater than k
             index-=1
         
         else:
-            lengthList[index]+=1
+            lengthList[index]=str(int(lengthList[index]+1))
             lengthList.pop(index-1)
             index-=1
-
+ 
     # here we will land at index=0
-    if lengthList[index]==k:
+    if int(lengthList[index])==k:
         num+=1
-
+ 
     return num
-
+ 
 testcase=int(input())
 for i in range(testcase):
     firstLine=input() # this line o
@@ -32,10 +32,11 @@ for i in range(testcase):
     numSnakes=int(firstLineList[0]) 
     numQueries=int(firstLineList[1])
     snakeLengthString=input()
-    snakeLengthList = list(map(int, snakeLengthString.split(' ')))
+    snakeLengthList=snakeLengthString.split(' ')
+    # snakeLengthList = list(map(int, snakeLengthString.split(' ')))
     # map function is used to map characters to integers in one go
     for j in range(numQueries):
         query=input()
         query=int(query)
         # print('list now:'+str(snakeLengthList))
-        print(answer(snakeLengthList, query))
+        print(answer(snakeLengthList, query)) 
