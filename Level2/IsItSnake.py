9bebc5e85aa640295f228dc09a2c2b1c06007c88
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-05-28 12:52:15 +0530

    looks like all sample test cases working for problem 2

--- Level2/IsItSnake.py
+++ Level2/IsItSnake.py
@@ -1,33 +1,42 @@
 def answer(row1, row2):
     columns=len(row1)
     snakeFound=0
+    stray=0
     snakeFoundColumn=0
-    for i in range(1,columns):
+    for i in range(columns-1):
         if row1[i]=='#':
-            if row1[i-1]=='#' and row2[i]=='#': # more than one snake
-                return 'no'
+            if row1[i+1]=='#' and row2[i]=='#':
+                stray+=1
+                snakeFound=1
+                continue
             if row2[i]=='#':
                 snakeFound=1
                 continue
-            elif row1[i-1]=='#':
+            elif row1[i+1]=='#':
                 snakeFound=1
                 continue
             else:
-                if snakeFound==1:
+                if i==0:
+                    return 'no'
+                elif row1[i-1]!='#':
                     return 'no'
                 else:
                     continue
         elif row2[i]=='#':
-            if row2[i-1]=='#' and row1[i]=='#': # more than one snake
-                return 'no'
+            if row2[i+1]=='#' and row1[i]=='#':
+                stray+=1
+                snakeFound=1
+                continue
             if row1[i]=='#':
                 snakeFound=1
                 continue
-            elif row2[i-1]=='#':
+            elif row2[i+1]=='#':
                 snakeFound=1
                 continue
             else:
-                if snakeFound==1:
+                if i==0:
+                    return 'no'
+                elif row2[i-1]!='#':
                     return 'no'
                 else:
                     continue
